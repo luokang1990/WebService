6ca59c1f649aaad6feb5e8367da84cad1892452b
luokang1990 <932120166@qq.com>
2017-12-07 14:41:10 +0800

    add nhao

--- fileop.cpp
+++ fileop.cpp
@@ -1,10 +1,10 @@
-#include "apue.h"
+﻿#include "apue.h"
 int main(void)
 {
   if (lseek(STDIN_FILENO,0,SEEK_CUR)==-1)
   	printf("cannot seek\n");
   else
-  	printf("seek ok\n");
+  	printf("seek ok 更改 nhao\n");
 
   exit(0);
   	
